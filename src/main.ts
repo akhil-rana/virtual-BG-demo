b07ff84415307acf51be3c5b371bc2ff292d3b16
akhil-rana <akhilrana0001@gmail.com>
2021-08-26 02:31:56 +0530

    ADD: proper blur + video stream

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,45 @@
 import './style.scss';
 import { start } from 'virtual-bg';
+import { canvasRGBA } from 'stackblur-canvas';
 
 const inputVideoElement: HTMLVideoElement =
   document.querySelector('#inputVideoElement')!;
+const outputVideoElement: HTMLVideoElement = document.querySelector(
+  '#outputVideoElement'
+)!;
 const toggleButton: any = document.querySelector('#toggleButton')!;
-
+const foregroundCanvasElement: HTMLCanvasElement = document.querySelector(
+  '.foreground_output_canvas'
+)!;
+const backgroundCanvasElement: HTMLCanvasElement = document.querySelector(
+  '.background_output_canvas'
+)!;
+const outputCanvasElement: HTMLCanvasElement =
+  document.querySelector('.output_canvas')!;
+const outputCanvasCtx: any = outputCanvasElement.getContext('2d');
+let myStream: MediaStream;
 let isSegmentationOn = false;
+let isCaptureStarted = false;
 
 toggleButton.onclick = async () => {
   isSegmentationOn ? null : start(inputVideoElement, onResults, 0);
   // isSegmentationOn = !isSegmentationOn;
 };
 
+function startStreamCapture() {
+  myStream = outputCanvasElement.captureStream();
+  outputVideoElement.srcObject = myStream;
+  isCaptureStarted = true;
+}
+
 function onResults(results: any) {
-  let foregroundCanvasElement: HTMLCanvasElement = document.querySelector(
-    '.foreground_output_canvas'
-  )!;
-  let backgroundCanvasElement: HTMLCanvasElement = document.querySelector(
-    '.background_output_canvas'
-  )!;
   showCanvas(results, foregroundCanvasElement, 'foreground');
   showCanvas(results, backgroundCanvasElement, 'background');
+  outputCanvasCtx.drawImage(backgroundCanvasElement, 0, 0);
+  outputCanvasCtx.drawImage(foregroundCanvasElement, 0, 0);
+  if (!isCaptureStarted) {
+    startStreamCapture();
+  }
 }
 
 function showCanvas(
@@ -39,13 +58,9 @@ function showCanvas(
     canvasElement.width,
     canvasElement.height
   );
-
-  canvasCtx.globalCompositeOperation =
-    type === 'background'
-      ? 'source-out'
-      : type === 'foreground'
-      ? 'source-in'
-      : 'source-in';
+  if (type === 'foreground') {
+    canvasCtx.globalCompositeOperation = 'source-in';
+  }
 
   canvasCtx.drawImage(
     results.image,
@@ -55,5 +70,15 @@ function showCanvas(
     canvasElement.height
   );
 
+  if (type === 'background') {
+    canvasRGBA(
+      canvasElement,
+      0,
+      0,
+      canvasElement.width,
+      canvasElement.height,
+      20
+    );
+  }
   canvasCtx.restore();
 }
