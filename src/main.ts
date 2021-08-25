4e0eb6200e29348699fd031b53ffc545ac829d67
akhil-rana <akhilrana0001@gmail.com>
2021-08-25 23:47:38 +0530

    FIX: build and own virtual-bg npm package

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,16 @@
 import './style.scss';
-import { SelfieSegmentation } from '@mediapipe/selfie_segmentation';
+import { start } from 'virtual-bg';
 
 const videoElement: HTMLVideoElement = document.querySelector('#videoElement')!;
 const toggleButton: any = document.querySelector('#toggleButton')!;
-// const containerElement: HTMLElement = document.getElementById('container')!;
 
 let canvasElement: HTMLCanvasElement =
   document.querySelector('.output_canvas')!;
 
 let isSegmentationOn = false;
-let myStream: MediaStream;
-let selfieSegmentation: SelfieSegmentation;
-
-async function start() {
-  selfieSegmentation = new SelfieSegmentation({
-    locateFile: (file) => {
-      return `https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/${file}`;
-    },
-  });
-  selfieSegmentation.setOptions({
-    modelSelection: 1,
-  });
-  selfieSegmentation.onResults(onResults);
-
-  myStream = await navigator.mediaDevices.getUserMedia({
-    video: { width: { ideal: 1024 }, height: { ideal: 720 } },
-  });
-
-  videoElement.srcObject = myStream;
-  videoElement.addEventListener('play', () => {
-    async function step() {
-      await selfieSegmentation.send({ image: videoElement });
-      requestAnimationFrame(step);
-    }
-    requestAnimationFrame(step);
-  });
-  videoElement.addEventListener('pause', async () => {});
-}
 
 toggleButton.onclick = async () => {
-  isSegmentationOn ? null : start();
+  isSegmentationOn ? null : start(videoElement, onResults);
   // isSegmentationOn = !isSegmentationOn;
 };
 
