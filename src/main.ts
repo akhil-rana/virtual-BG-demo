740e2c7f7bd54df607375be78c88b097a5eca0c1
akhil-rana <akhilrana0001@gmail.com>
2021-08-26 20:06:00 +0530

    ADD: camera input

--- src/main.ts
+++ src/main.ts
@@ -10,5 +10,11 @@ const outputCanvasElement: HTMLCanvasElement | any =
   document.querySelector('.output_canvas')!;
 
 toggleButton.onclick = async () => {
+  let myStream = await navigator.mediaDevices.getUserMedia({
+    video: { width: { ideal: 1024 }, height: { ideal: 720 } },
+  });
+
+  inputVideoElement.srcObject = myStream;
+
   blurBackground(inputVideoElement, outputCanvasElement, 7);
 };
