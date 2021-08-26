9d5efd0cfeb08def7f6848947f026aa051dc3bd7
akhil-rana <akhilrana0001@gmail.com>
2021-08-26 20:18:29 +0530

    ADD: improvements

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,14 @@ toggleButton.onclick = async () => {
     video: { width: { ideal: 1024 }, height: { ideal: 720 } },
   });
 
+  const width = myStream.getVideoTracks()[0].getSettings().width;
+  const height = myStream.getVideoTracks()[0].getSettings().height;
+
   inputVideoElement.srcObject = myStream;
+
+  outputCanvasElement.style.display = 'unset';
+  outputCanvasElement.style.aspectRatio = `${width}/${height}`;
+  toggleButton.style.display = 'none';
 
   blurBackground(inputVideoElement, outputCanvasElement, 7);
 };
