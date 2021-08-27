a971d212b87a3c3801b017b7250f5a4915c0871b
akhil-rana <akhilrana0001@gmail.com>
2021-08-27 20:44:08 +0530

    ADD: image background and updated virtual-bg

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,27 @@
 import './style.scss';
-import { segmentBackground, applyBlur } from 'virtual-bg';
+import { segmentBackground, applyBlur, applyImageBackground } from 'virtual-bg';
 
 const inputVideoElement: HTMLVideoElement =
   document.querySelector('#inputVideoElement')!;
 
-const toggleButton: any = document.querySelector('#toggleButton')!;
+const toggleButtonElement: any = document.querySelector('#toggleButton')!;
 
 const outputCanvasElement: HTMLCanvasElement | any =
   document.querySelector('.output_canvas')!;
 
-const canvasContainerElement: HTMLDivElement | any =
-  document.querySelector('#canvas-container')!;
+const containerElement: HTMLDivElement | any =
+  document.querySelector('#container')!;
 
-const blurIntensitySlider: HTMLInputElement | any = document.querySelector(
-  '#blurIntensitySlider'
-)!;
+const effectTypeSelectorElement: HTMLSelectElement | any =
+  document.querySelector('#effectTypeSelector')!;
 
-toggleButton.onclick = async () => {
+const blurIntensitySliderElement: HTMLInputElement | any =
+  document.querySelector('#blurIntensitySlider')!;
+
+const imageBrowserInputElement: HTMLInputElement | any =
+  document.querySelector('#imageBrowserInput')!;
+
+toggleButtonElement.onclick = async () => {
   let myStream = await navigator.mediaDevices.getUserMedia({
     video: { width: { ideal: 1920 }, height: { ideal: 1080 } },
   });
@@ -32,18 +37,56 @@ toggleButton.onclick = async () => {
 
   inputVideoElement.srcObject = myStream;
 
-  canvasContainerElement.style.display = 'unset';
+  containerElement.style.display = 'unset';
   outputCanvasElement.style.width = width;
   outputCanvasElement.style.height = height;
-
   outputCanvasElement.style.aspectRatio = `${width}/${height}`;
-  toggleButton.style.display = 'none';
+  toggleButtonElement.style.display = 'none';
 
   segmentBackground(inputVideoElement, outputCanvasElement);
   applyBlur(7);
 };
 
-blurIntensitySlider.onchange = (e: any) => {
+blurIntensitySliderElement.onchange = (e: any) => {
   const blurIntensity = e?.target?.value;
   applyBlur(blurIntensity);
 };
+
+effectTypeSelectorElement.onchange = (e: any) => {
+  const type = e?.target?.value;
+
+  if (type === 'blur') {
+    (<HTMLDivElement>(
+      document.querySelector('#blurIntensityContainer')
+    )).style.display = 'unset';
+    (<HTMLDivElement>(
+      document.querySelector('#imageBrowserContainer')
+    )).style.display = 'none';
+    applyBlur(7);
+  } else if (type === 'image') {
+    (<HTMLDivElement>(
+      document.querySelector('#blurIntensityContainer')
+    )).style.display = 'none';
+    (<HTMLDivElement>(
+      document.querySelector('#imageBrowserContainer')
+    )).style.display = 'unset';
+    if (imageBrowserInputElement?.files[0]) {
+      setBackgroundImage(imageBrowserInputElement?.files[0]);
+    } else
+      imageBrowserInputElement.onchange = (e: any) => {
+        applyBlur(0);
+        setBackgroundImage(e?.target?.files[0]);
+      };
+  }
+};
+
+function setBackgroundImage(imageFile: File) {
+  const reader = new FileReader();
+  reader.readAsDataURL(imageFile);
+  reader.onload = (e: any) => {
+    const imageUrl = e?.target?.result;
+    const image = new Image();
+    image.src = imageUrl;
+    applyImageBackground(image);
+  };
+}
